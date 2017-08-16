615ebcbd125ffb75cc92cf835f2b2658e76be63e
anujeet98 <31065438+anujeet98@users.noreply.github.com>
2017-08-16 21:31:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send('article one is requested);
+  res.send('article one is requested');
 });
 
 app.get('/article-two', function (req, res) {
-  res.send('article two is requested);
+  res.send('article two is requested');
 });
 
 app.get('/article-three', function (req, res) {
-  res.send('article three is requested);
+  res.send('article three is requested');
 });
 
 app.get('/ui/style.css', function (req, res) {
