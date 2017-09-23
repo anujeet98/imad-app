ae8f7e62163832dfb51e23695f721490a447e2bf
anujeet98 <31065438+anujeet98@users.noreply.github.com>
2017-09-23 12:31:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articl one = {
+  title : "article-one| anujeet swain",
+  heading: "article-one",
+  date: "sept 5 2017",
+  content: `<p>
+                this is my web app.going to add more content.this is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more content
+            </p>
+            <p>
+                this is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more content.
+            </p>
+            <p>
+                this is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more contentthis is my web app.going to add more content"
+            </p>`   
+};
+function createTemplate (data) {
+  var title=data.title;
+  var date=data.date;
+  var heading=data.heading;
+  var content=data.content;
+  var htmlTemplate =`<html>
+    <head>
+        <title>$(title)
+        </title>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+          <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+     <div class="container">  
+        <div>
+            <a href="/">HOME</a>
+        </div>
+        <div>
+        
+            $(heading)
+        </div>
+        <div>
+            $ (date)
+        </div>
+        <div>
+           $(content)
+        </div>
+     </div>    
+    </body>
+</html>
+
+`;    
+return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articlOne));
 });
 
 app.get('/article-two', function (req, res) {
