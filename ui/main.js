88143c77ffa382244d39020e22e6703311d07711
anujeet98 <31065438+anujeet98@users.noreply.github.com>
2017-09-23 15:12:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-element.innerHTML = 'New Value';
-
-//move thew element
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight () {
-    marginLeft = marginLeft+1;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick = function() {
-    var interval = setInterval(moveRight,100);
+//counter code
+var button = document.getElementById('counter');
+button.onclick=function () {
+    //make a request to a counter endpoint
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.aonreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            // take some action
+            if(request.status ===200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }    
+        }
+        //not yet done
+    };
+    //make the request 
+    request.open('GET','http://anujeet98.imad.hasura-app.io',true);
+    request.send(null);
 };
